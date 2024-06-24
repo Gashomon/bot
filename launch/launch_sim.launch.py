020bcca904b8acd0aa56e02c24e236fd8ac242ef
Gashomon <142756955+Gashomon@users.noreply.github.com>
2024-06-24 13:11:47 +0800

    launch gazebo with imported world

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]), launch_arguments={'world': './src/bot/worlds/test_world.world'}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
