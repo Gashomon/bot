b973f22f87e42750a92022a2793b0cedd76b6119
Gashomon <142756955+Gashomon@users.noreply.github.com>
2024-10-27 00:06:17 +0800

    testing

--- implement_bot/hardware/diffbot_system.cpp
+++ implement_bot/hardware/diffbot_system.cpp
@@ -229,7 +229,7 @@ hardware_interface::return_type NewHardwareInterface::DiffBotHardwareSystem::wri
 
   int motor_l_counts_per_loop = wheel_l_.cmd / wheel_l_.rads_per_count / cfg_.loop_rate;
   int motor_r_counts_per_loop = wheel_r_.cmd / wheel_r_.rads_per_count / cfg_.loop_rate;
-  // comms_.set_motor_values(motor_l_counts_per_loop, motor_r_counts_per_loop);
+  comms_.set_motor_values(motor_l_counts_per_loop, motor_r_counts_per_loop);
   // comms_.run_motor_pwm(20, 20);
 
   return hardware_interface::return_type::OK;
